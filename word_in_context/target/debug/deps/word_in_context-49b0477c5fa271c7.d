407dd8662161f77edc26b7309a21dd312b42d128
arthur9548 <tucobotelho@gmail.com>
2025-05-29 20:51:56 -0300

    formatting and removing warnings

---
+++ word_in_context/target/debug/deps/word_in_context-49b0477c5fa271c7.d
@@ -0,0 +1,5 @@
+/home/arthur/dev/UnB/TP2/TP2_codigos/word_in_context/target/debug/deps/libword_in_context-49b0477c5fa271c7.rmeta: src/main.rs
+
+/home/arthur/dev/UnB/TP2/TP2_codigos/word_in_context/target/debug/deps/word_in_context-49b0477c5fa271c7.d: src/main.rs
+
+src/main.rs:
--- word_in_context/target/debug/word_in_context.d
+++ word_in_context/target/debug/word_in_context.d
@@ -1,1 +1,1 @@
-C:\Users\rafae\Rafael_Mateus\Rafael\UNB\faculdade\4\ Semestre\TP2\TP2_codigos\word_in_context\target\debug\word_in_context.exe: C:\Users\rafae\Rafael_Mateus\Rafael\UNB\faculdade\4\ Semestre\TP2\TP2_codigos\word_in_context\src\main.rs
+/home/arthur/dev/UnB/TP2/TP2_codigos/word_in_context/target/debug/word_in_context: /home/arthur/dev/UnB/TP2/TP2_codigos/word_in_context/src/main.rs
