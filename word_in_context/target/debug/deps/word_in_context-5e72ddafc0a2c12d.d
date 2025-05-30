2036d53b2f3dc425748408386e863628973e1caa
Rafael <rafaelsapienzapinheiro@gmail.com>
2025-05-30 14:57:21 -0300

    feat: allow input and stop words paths via command line

---
+++ word_in_context/target/debug/deps/word_in_context-5e72ddafc0a2c12d.d
@@ -0,0 +1,7 @@
+C:\Users\rafae\Rafael_Mateus\Rafael\UNB\faculdade\4 Semestre\TP2\TP2_codigos\word_in_context\target\debug\deps\libword_in_context-5e72ddafc0a2c12d.rmeta: src\lib.rs
+
+C:\Users\rafae\Rafael_Mateus\Rafael\UNB\faculdade\4 Semestre\TP2\TP2_codigos\word_in_context\target\debug\deps\libword_in_context-5e72ddafc0a2c12d.rlib: src\lib.rs
+
+C:\Users\rafae\Rafael_Mateus\Rafael\UNB\faculdade\4 Semestre\TP2\TP2_codigos\word_in_context\target\debug\deps\word_in_context-5e72ddafc0a2c12d.d: src\lib.rs
+
+src\lib.rs:
--- word_in_context/target/debug/libword_in_context.d
+++ word_in_context/target/debug/libword_in_context.d
@@ -1,1 +1,1 @@
-/home/arthur/dev/UnB/TP2/TP2_codigos/word_in_context/target/debug/libword_in_context.rlib: /home/arthur/dev/UnB/TP2/TP2_codigos/word_in_context/src/lib.rs
+C:\Users\rafae\Rafael_Mateus\Rafael\UNB\faculdade\4\ Semestre\TP2\TP2_codigos\word_in_context\target\debug\libword_in_context.rlib: C:\Users\rafae\Rafael_Mateus\Rafael\UNB\faculdade\4\ Semestre\TP2\TP2_codigos\word_in_context\src\lib.rs
